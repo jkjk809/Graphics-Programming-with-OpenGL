bc4285105128c07eb2ca1343e7aada0e66ee8dd0
jkjk809 <jaydenk504@gmail.com>
2024-04-20 13:41:28 -0500

    bnalashahlsk

--- Source/graphics/Model.h
+++ Source/graphics/Model.h
@@ -4,6 +4,11 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <vector>
@@ -13,16 +18,31 @@
 class Model
 {
 public:
+	glm::vec3 pos;
+	glm::vec3 size;
+
 	std::vector<Mesh> meshes;
 
 	Model();
-
+	Model(glm::vec3 pos = glm::vec3(0.0f), glm::vec3 size = glm::vec3(1.0f));
 	//ovverridden
 	void init();
+
+	void loadModel(std::string path);
 
 	void render(Shader shader);
 
 	void cleanup();
+
+protected:
+	std::vector<Mesh> meshes;
+	std::string directory;
+	std::vector<Texture> textures_loaded;
+
+	void processNode(aiNode* node, const aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+	std::vector<Texture> loadTextures(aiMaterial* mat, aiTextureType type);
+
 };
 
 
